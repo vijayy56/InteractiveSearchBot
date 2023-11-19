1a7dcff495bcf7c4987a8fd5ce0e2fbdf14a0c7f
vijayy56 <vijay.vissu56@gmail.com>
2023-11-19 14:40:20 -0600

    openai model

--- response.py
+++ response.py
@@ -2,13 +2,14 @@ import chainlit as cl
 import os 
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import DirectoryLoader
 from langchain.document_loaders import UnstructuredPowerPointLoader
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader,PyPDFLoader
 
 @cl.on_message
 async def main(message: cl.Message):
@@ -21,13 +22,13 @@ def llm(query):
     os.environ["OPENAI_API_KEY"] = "API_KEY"    
     persist_directory = 'db'
     embedding = OpenAIEmbeddings()
-    loadResourceDocuments();
+    # loadResourceDocuments();
     vectordb = Chroma(persist_directory=persist_directory, 
                     embedding_function=embedding)
 
     retriever = vectordb.as_retriever(search_kwargs={"k": 2})
 
-    qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(), 
+    qa_chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo-0613"), 
                                     chain_type="stuff", 
                                     retriever=retriever, 
                                     return_source_documents=True)
@@ -39,12 +40,12 @@ def llm(query):
 
 
 def loadResourceDocuments():
-    loader2 = DirectoryLoader("/Users/vijay/Desktop/Semester 1/Network Security/Lectures",loader_cls=UnstructuredPowerPointLoader)
+    documents=[]
+    # loader2 = DirectoryLoader("/Users/vijay/Desktop/Semester 1/Network Security/Lectures",loader_cls=UnstructuredPowerPointLoader)
     # loader = DirectoryLoader("/Users/vijay/Desktop/Semester 1/Network Security/TB", glob='**/*.pdf',loader_cls=UnstructuredPDFLoader);
-    documents = loader2.load()
-
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    loader2 = PyPDFLoader("/Users/vijay/Desktop/Semester 1/Network Security/TB/Stallingstest.pdf")
+    documents.extend(loader2.load())
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100,separator="\n")
     texts = text_splitter.split_documents(documents)
     vectordb = None
     embedding = OpenAIEmbeddings()
